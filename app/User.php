ffd0d8745eb188d8fc2925fce28e33c71223be70
hvsharma63 <hvsharma63@gmail.com>
2019-03-06 23:54:35 +0530

    Factory and Seed

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Question;
 
 class User extends Authenticatable
 {
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -1,9 +1,20 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Category;
+use App\User;
 
 $factory->define(App\Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug'  => str_slug($title),
+        'body'  => $faker->text,
+        'user_id' => function () {
+            return User::all()->random();
+        },
+        'category_id' => function () {
+            return Category::all()->random();
+        }
     ];
 });
