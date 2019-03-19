bac86848d9af1d439338026639ef846388094724
hvsharma63 <hvsharma63@gmail.com>
2019-03-19 14:15:16 +0530

    Vue & Vuetify Started

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,17 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
+import Vue from "vue";
+import Vuetify from "vuetify";
+
+Vue.use(Vuetify);
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -20,7 +23,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,5 +35,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: "#app"
 });
---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <div id="app">
+        <v-app>
+            <template>
+                <v-toolbar>
+                    <v-toolbar-side-icon></v-toolbar-side-icon>
+                    <v-toolbar-title>Title</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items class="hidden-sm-and-down">
+                    <v-btn flat>Link One</v-btn>
+                    <v-btn flat>Link Two</v-btn>
+                    <v-btn flat>Link Three</v-btn>
+                    </v-toolbar-items>
+                </v-toolbar>
+                </template>
+        </v-app>
+    </div>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
