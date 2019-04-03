7b0f28b2668fd7975eb869c989994a1eb9e8254e
hvsharma63 <hvsharma63@gmail.com>
2019-04-03 18:00:19 +0530

    upto lesson 40

---
+++ resources/js/components/login/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-form v-model="valid">
+      <v-text-field v-model="form.email" type="email" label="E-mail" required></v-text-field>
+      <v-text-field v-model="form.password" type="password" label="Password" required></v-text-field>
+      <v-btn color="green" type="submit">Login</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: null,
+        password: null
+      }
+    };
+  }
+};
+</script>
+
+<style>
+</style>
